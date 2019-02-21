3442d86581a134024ca34436a69c5e448b33801a
weijlu <weijlu@ebay.com>
2019-02-21 13:33:15 +0800

    upgrade pom dependency version

--- src/main/java/com/rovger/entity/City.java
+++ src/main/java/com/rovger/entity/City.java
@@ -4,6 +4,10 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * @Description: TODO
@@ -51,4 +55,11 @@ public class City implements Serializable {
      */
     @NonNull
     private String description;
+
+    public static void main(String[] args) {
+        /*System.out.println((int)Math.floor(53644 * 1.0/10000));
+        System.out.println(15/4);*/
+        String str = "ISD090774J43565765.txt";
+        System.out.println(Pattern.matches("^(IS0026256L|ISD090774J).*", str));
+    }
 }
